02a8a31944d835537f4e9801f979079a51cf24bb
Minchu1 <73154863+Minchu1@users.noreply.github.com>
2021-02-04 19:24:05 +1300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -26,33 +26,46 @@ start(){
         form.display();
 
     }
+    car1=createSprite(100,200,50,50);
+    car2=createSprite(300,200,50,50);
+    car3=createSprite(500,200,50,50);
+    car4=createSprite(700,200,50,50);
+    cars=[car1,car2,car3,car4];
+
 }
 play(){
     form.hide();
     textSize(24);
-    text("game has started!", 400,100);
+    text("game has started!", displayWidth/2,100);
     Player.getPlayerInfo();
 
     if(allP!==undefined){
-        var displayPosition=150;
+        var index=0;
+        var x=0;
+        var y=0;
         for(var plr in allP){
-            if(plr==="player"+player.index){
-                fill("red");
-            
+           index=index+1;
+           x=x+200;
+           y=displayHeight-allP[plr].distance;
+           cars[index-1]=x;
+           cars[index-1]=y;
+            if(index===player.index){
+
+                //cars[index-1].shapeColor="red";
+                camera.position.x=displayWidth/2;
+                camera.position.y=cars[index-1].y;
+
             }
 
-            else{
-                fill("black");
-            }
-            displayPosition+=20;
-            textSize(15);
-            text(allP[plr].name+": "+allP[plr].distance,400,displayPosition);
-
+        
+           
         }
     }
-    if(keyDown(UP_ARROW)&&player.index!==null){
+    if(keyIsDown("w")&&player.index!==null){
         player.distance+=5;
         player.update();
     }
+    drawSprites();
 }
+
 }
